36a8bd753269dbd7b302926646c2f4fa745cbff3
Penqle <flpenq@gmail.com>
2026-04-30 19:29:22 -0700

    Added proc event for Blinding Haze

--- sql/wip_updates/Rogue/spel_proc_event.sql
+++ sql/wip_updates/Rogue/spel_proc_event.sql
@@ -37,3 +37,23 @@ VALUES
 (52535, 0, 8, 16777216, 0, 0, 4096, 524288, 0, 0, 0),
 (52536, 0, 8, 16777216, 0, 0, 4096, 524288, 0, 0, 0),
 (52537, 0, 8, 16777216, 0, 0, 4096, 524288, 0, 0, 0);
+
+-- Blinding Haze Proc event
+REPLACE INTO spell_proc_event
+(
+    entry,
+    SchoolMask,
+    SpellFamilyName,
+    SpellFamilyMask0,
+    SpellFamilyMask1,
+    SpellFamilyMask2,
+    procFlags,
+    procEx,
+    ppmRate,
+    CustomChance,
+    Cooldown
+)
+VALUES
+(51680, 0, 8, 4096, 0, 0, 65536, 0, 0, 0, 0),
+(51681, 0, 8, 4096, 0, 0, 65536, 0, 0, 0, 0),
+(51682, 0, 8, 4096, 0, 0, 65536, 0, 0, 0, 0);
