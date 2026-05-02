e10bd0596914ca9acac21bb4cd7cfe4d9001ab99
Penqle <flpenq@gmail.com>
2026-05-01 21:05:31 -0700

    Adding spell_affect entry for Rogue talent Aggression

--- sql/wip_updates/Rogue/spell_affect.sql
+++ sql/wip_updates/Rogue/spell_affect.sql
@@ -10,3 +10,16 @@ VALUES
 (52507, 1, 4294967296),
 (52508, 0, 4294967296),
 (52508, 1, 4294967296);
+
+-- Aggression
+-- Sinister Strike (0x2), Eviscerate (0x20000), Riposte (0x80000000), Surprise Attack (0x200000000)
+REPLACE INTO spell_affect
+(
+    entry,
+    effectId,
+    SpellFamilyMask
+)
+VALUES
+(18427, 0, 10737549314),
+(18428, 0, 10737549314),
+(18429, 0, 10737549314);
