8595acb7fb412318eb9d3aed7b17754549915159
Penqle <flpenq@gmail.com>
2026-05-01 19:43:16 -0700

    Added spell_affect for Blade Rush

---
+++ sql/wip_updates/Rogue/spell_affect.sql
@@ -0,0 +1,12 @@
+-- Blade Rush
+REPLACE INTO spell_affect
+(
+    entry,
+    effectId,
+    SpellFamilyMask
+)
+VALUES
+(52507, 0, 4294967296),
+(52507, 1, 4294967296),
+(52508, 0, 4294967296),
+(52508, 1, 4294967296);
