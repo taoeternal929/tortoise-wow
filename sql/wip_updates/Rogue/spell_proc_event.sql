ce989a732fa0ebcd65125a6e7e97fa180119563e
Penqle <flpenq@gmail.com>
2026-05-02 22:46:48 -0700

    Fixed cooldown behavior for Vigor
    The original spell is using aura 109 SPELL_AURA_ADD_TARGET_TRIGGER, which bypasses cooldowns
    
    I initially went to make aura 109 beholden to cooldowns
    
    I checked all other spell effects that are using aura 109 but found that none of them should be beholden to cooldowns
    
    Going to keep an eye on this just in case we need to change the behavior of aura 109, but this implementation works as intended

--- sql/wip_updates/Rogue/spell_proc_event.sql
+++ sql/wip_updates/Rogue/spell_proc_event.sql
@@ -17,3 +17,22 @@ VALUES
 (14174, 0, 8, 1048576, 0, 0, 16, 524288, 0, 0, 0),
 (14175, 0, 8, 1048576, 0, 0, 16, 524288, 0, 0, 0),
 (14176, 0, 8, 1048576, 0, 0, 16, 524288, 0, 0, 0);
+
+-- Vigor
+REPLACE INTO spell_proc_event
+(
+    entry,
+    SchoolMask,
+    SpellFamilyName,
+    SpellFamilyMask0,
+    SpellFamilyMask1,
+    SpellFamilyMask2,
+    procFlags,
+    procEx,
+    ppmRate,
+    CustomChance,
+    Cooldown
+)
+VALUES
+(14983, 0, 8, 268558336, 0, 0, 65536, 0, 0, 50, 8),
+(52527, 0, 8, 268558336, 0, 0, 65536, 0, 0, 100, 8);
