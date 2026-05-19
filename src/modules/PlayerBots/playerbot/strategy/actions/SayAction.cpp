9e4e408a2c347909fc7e347eecc9eff0db0c097d
Rundao Lu <lurundao@gmail.com>
2026-05-19 04:17:16 +0000

    Fix a few CMake link/install issues, and boost lib issues

--- src/modules/PlayerBots/playerbot/strategy/actions/SayAction.cpp
+++ src/modules/PlayerBots/playerbot/strategy/actions/SayAction.cpp
@@ -442,7 +442,9 @@ delayedPackets ChatReplyAction::GenerateResponsePackets(const std::string json
     if (!debugLines.empty())
     {
         debugPackets = LinesToPackets(debugLines, systemTemplate, true, 1);
-        packets.insert(packets.begin(), debugPackets.begin(), debugPackets.end());
+	packets.insert(packets.begin(),
+               std::make_move_iterator(debugPackets.begin()),
+               std::make_move_iterator(debugPackets.end()));
     }
 
     return packets;
